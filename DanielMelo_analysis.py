f33893fb5fbf95d55780282069eaf45c5ec75bd7
AdminBiocomputo <dg.melo@uniandes.edu.co>
2018-03-20 16:53:49 -0500

    comienzo2

--- DanielMelo_analysis.py
+++ DanielMelo_analysis.py
@@ -2,6 +2,8 @@ import numpy as np
 
 import math
 
+import scipy
+
 def normal_dist(x, mean, sigma):
 
     a = math.sqrt(1/(2*np.pi*sigma*sigma))
@@ -13,6 +15,19 @@ def normal_dist(x, mean, sigma):
 def get_fit(filename):
     
     archivo = np.loadtxt(files[i])
+
+    freqs, bins = np.histogram(archivo, named=true)
+
+    y = freqs
+
+    x = 0.5*(bins[:-1] + bins[1:])
+
+    q, w = scipy.optimize.curve_fit(normal_dist, y, x)
+
+    print "El ajuste de prom es " + str(q) + " y el ajuste de sigma es " + w
+
+
+
 
         
 
